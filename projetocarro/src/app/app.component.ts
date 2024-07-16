23a9e0cfa4f5eec881209dc59f90ecf1e9ccdcdb
Isaque <isaquebatista400@gmail.com>
2024-07-16 14:23:26 -0300

    closes #4 - Criando componente cars

--- projetocarro/src/app/app.component.ts
+++ projetocarro/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CarComponent } from './components/car/car.component';
+import { CarsComponent } from './components/cars/cars.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { CarComponent } from './components/car/car.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    CarComponent
+    CarComponent,
+    CarsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
