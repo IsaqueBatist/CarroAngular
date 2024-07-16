740fbd41a03e9b17d128c74bb3c5a829d3b719fe
Isaque <isaquebatista400@gmail.com>
2024-07-16 14:21:34 -0300

    closes #3 - Fazendo o layout do formulario

--- projetocarro/src/app/app.component.ts
+++ projetocarro/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CarComponent } from './components/car/car.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    CarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
