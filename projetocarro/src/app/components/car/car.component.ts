73e071991bd9b0e9176aa8b826e1e7330db3c2df
Isaque <isaquebatista400@gmail.com>
2024-07-16 15:10:32 -0300

    closes #9 - Ligando o objeto ao formulario

--- projetocarro/src/app/components/car/car.component.ts
+++ projetocarro/src/app/components/car/car.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ICar } from '../../icar';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-car',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
 export class CarComponent {
-
+  car:ICar = {
+    name: "Isaque",
+    montadora: 'aquela',
+    price: 1223,
+    year: 2000
+  }
 }
